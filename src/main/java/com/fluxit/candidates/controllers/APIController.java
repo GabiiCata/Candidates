afb95d0bbf8c1a5274da24f2f9f96425be2ed636
GabiiCata <gabiicata94@gmail.com>
2020-06-08 00:51:44 -0300

    api - add new candidate

--- src/main/java/com/fluxit/candidates/controllers/APIController.java
+++ src/main/java/com/fluxit/candidates/controllers/APIController.java
@@ -3,12 +3,17 @@ package com.fluxit.candidates.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fluxit.candidates.interfaceServices.ICandidateService;
@@ -38,6 +43,19 @@ public class APIController {
 			return ResponseEntity.ok( "Candidate eliminated" );
 		}catch (Exception e) {
 			return new ResponseEntity<>("Candidate doesn't exist", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	
+	@PostMapping("/candidate")
+	public Object addCandidate ( @RequestBody Candidate c )
+	{
+		try
+		{
+			service.save(c);
+			return service.listById( c.getId() );
+		}catch (Exception e) {
+			return new ResponseEntity<>("Error adding new candidate", HttpStatus.BAD_REQUEST);
 		}
 	}
 }
