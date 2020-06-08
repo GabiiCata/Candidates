09bfc41b90f9900545f4716d800631aa82104251
GabiiCata <gabiicata94@gmail.com>
2020-06-07 21:03:50 -0300

    lombok impl#

--- src/main/java/com/fluxit/candidates/CandidatesApplication.java
+++ src/main/java/com/fluxit/candidates/CandidatesApplication.java
@@ -1,4 +1,4 @@
-package com.fluxit.Candidates;
+package com.fluxit.candidates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/fluxit/candidates/models/Candidate.java
@@ -0,0 +1,14 @@
+package com.fluxit.candidates.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Candidate {
+	
+	
+	private int id;
+	private String name;
+	private String phone;
+
+}
